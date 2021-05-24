4afd5b7e0333ad1bb5c602cb9b5b9ef9ad0e89fb
Kaushal1011 <kaushalpatil10@gmail.com>
2021-05-25 03:31:11 +0530

    created wordcloud and sentiment analysis did

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from elastic import *
 from twitter import *
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+# from state import sentiment_count, emotion_count
 # from sentiment import sentiment_classifier
 
 # Emotion Detection
@@ -48,6 +51,13 @@ def update_output(n_clicks, input1):
     # print(es_index(tweets[0]))
     # es_bulk_index(tweets)
     es_add(tweets)
+    return("Added Tweets to Elastic {}".format(n_clicks))
+
+
+@app.callback(Output('output-state2', 'children'),
+              Input('analyse-submit', 'n_clicks'),
+              State('input-1-state', 'value'))
+def update_output(n_clicks, input1):
 
     res = es.search(index="tweet_v2", body={
         "size": 100,
@@ -70,20 +80,84 @@ def update_output(n_clicks, input1):
                     "field": "tweet.text.keyword", "size": 25
                 }
             }
-        }
+        }}, ignore=[400, 404])
+    # print(res)
+    tweettext = [text["_source"]["tweet"]["text"]
+                 for text in res["hits"]["hits"]]
 
+    # call sentiment api
+    keywords = [word["key"]
+                for word in res["aggregations"]["keywords"]["buckets"]]
+    score = [int(10000*word["score"])
+             for word in res["aggregations"]["keywords"]["buckets"]]
+
+    # Word Cloud Processing
+
+    wc_dict = {}
+    for i in range(len(keywords)):
+        wc_dict[keywords[i]] = score[i]
+
+    wordcloud = WordCloud(
+        width=500, height=500).generate_from_frequencies(wc_dict)
+    plt.figure(figsize=(15, 8))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    # plt.show()
+    plt.savefig('yourfile.png', bbox_inches='tight')
+    plt.close()
+
+    # sentiment processing
+
+    url = "http://localhost:8000/sentiment"
+
+    payload = json.dumps({
+        "textlist": tweettext
     })
-    print(res)
+    headers = {
+        'Content-Type': 'application/json',
+        'Cookie': 'csrftoken=Cu0qDKmbrZNCk1yDORM52IrPx9ylB8fmdiKrFrrqXTf0qt67BgL67h71q0aqR2zM'
+    }
 
-    # data = query({"inputs": "I like you. I love you"})
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-    # print(data)
-    # for tweet in tweets:
-    #     print(tweet.text)
-    return("Added Tweets to Elastic")
-    # return u'''
-    # {}
-    # '''.format(input1)
+    # proces sentiment values here
+    sentiment_count = {
+        "POSITIVE": 0,
+        "NEGATIVE": 0
+    }
+    for i in response.json()["data"][0]:
+        sentiment_count[i["label"]] += 1
+
+    print(sentiment_count)
+
+    # Emotion Process
+
+    url = "http://localhost:8000/emotion"
+
+    payload = json.dumps({
+        "textlist": tweettext
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Cookie': 'csrftoken=Cu0qDKmbrZNCk1yDORM52IrPx9ylB8fmdiKrFrrqXTf0qt67BgL67h71q0aqR2zM'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    # Process emotion data here
+    emotion_count = {
+        "sadness": 0,
+        "joy": 0,
+        "love": 0,
+        "anger": 0,
+        "fear": 0,
+        "surprise": 0
+    }
+
+    for i in response.json()["data"][0]:
+        emotion_count[i["label"]] += 1
+
+    print(emotion_count)
 
 
 if __name__ == '__main__':
--- state.py
+++ state.py
@@ -1,8 +1,12 @@
-positive = 0
-negtive = 0
-sadness = 0
-joy = 0
-love = 0
-anger = 0
-fear = 0
-surprise = 0
+emotion_count = {
+    "sadness": 0,
+    "joy": 0,
+    "love": 0,
+    "anger": 0,
+    "fear": 0,
+    "surprise": 0
+}
+sentiment_count = {
+    "POSITIVE": 0,
+    "NEGATIVE": 0
+}
